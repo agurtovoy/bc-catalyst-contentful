dd01cda4e8bebb571e5bd36fba257434af763146
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-08-02 14:46:47 -0500

    Add Contentful integration

--- app/[locale]/(default)/(faceted)/category/[slug]/page.tsx
+++ app/[locale]/(default)/(faceted)/category/[slug]/page.tsx
@@ -17,6 +17,9 @@ import { CategoryViewed } from './_components/category-viewed';
 import { SubCategories } from './_components/sub-categories';
 import { getCategoryPageData } from './page-data';
 
+import { getCategoryContent } from '~/contentful-client/queries/get-category-content';
+import CmsContent from '~/components/cms/cms-content';
+
 interface Props {
   params: {
     slug: string;
@@ -68,6 +71,9 @@ export default async function Category({ params: { locale, slug }, searchParams 
   const products = productsCollection.items;
   const { hasNextPage, hasPreviousPage, endCursor, startCursor } = productsCollection.pageInfo;
 
+  const catPath = category.path.replace(/(\/$)/, '').replace(/^\//, '');
+  const cmsContent = await getCategoryContent('category', catPath);
+
   return (
     <div className="group">
       <Breadcrumbs category={category} />
@@ -100,6 +106,8 @@ export default async function Category({ params: { locale, slug }, searchParams 
             </div>
           </div>
         </div>
+
+        {cmsContent.length > 0 && <CmsContent blocks={cmsContent} />}
 
         <div className="grid grid-cols-4 gap-8">
           <FacetedSearch
--- app/[locale]/(default)/product/[slug]/page.tsx
+++ app/[locale]/(default)/product/[slug]/page.tsx
@@ -17,6 +17,9 @@ import { Reviews } from './_components/reviews';
 import { Warranty } from './_components/warranty';
 import { getProduct } from './page-data';
 
+import { getCategoryContent } from '~/contentful-client/queries/get-category-content';
+import CmsContent from '~/components/cms/cms-content';
+
 interface ProductPageProps {
   params: { slug: string; locale: LocaleType };
   searchParams: Record<string, string | string[] | undefined>;
@@ -83,6 +86,8 @@ export default async function Product({ params, searchParams }: ProductPageProps
 
   const category = removeEdgesAndNodes(product.categories).at(0);
 
+  const cmsContent = await getCategoryContent('product', product.sku);
+
   return (
     <>
       {category && <Breadcrumbs category={category} />}
@@ -94,6 +99,7 @@ export default async function Product({ params, searchParams }: ProductPageProps
         >
           <Gallery noImageText={t('noGalleryText')} product={product} />
           <Details product={product} />
+          {cmsContent.length > 0 && <CmsContent blocks={cmsContent} className="lg:col-span-2 mx-8" />}
           <div className="lg:col-span-2">
             <Description product={product} />
             <Warranty product={product} />
